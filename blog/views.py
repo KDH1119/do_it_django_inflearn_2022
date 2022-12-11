adbf17bceb370369a57c8618e1f1af989767b926
김덕현 <kdh@Hyeons-MacBook-Air.local>
2022-12-11 21:25:00 +0900

    포스트 목록 페이지에 대표 이미지 출력하기

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from .models import Post
-from django.views.generic import ListView
 
 
 class PostList(ListView):
     model = Post
     ordering = '-pk'
 
+
+class PostDetail(DetailView):
+    model = Post
 
 # def index(request):
 #     posts = Post.objects.all().order_by('-pk')
@@ -20,13 +23,13 @@ class PostList(ListView):
 #     )
 
 
-def single_post_page(request, pk):
-    post = Post.objects.get(pk=pk)
-
-    return render(
-        request,
-        'blog/single_page.html',
-        {
-            'post': post,
-        }
-    )
+# def single_post_page(request, pk):
+#     post = Post.objects.get(pk=pk)
+#
+#     return render(
+#         request,
+#         'blog/post_detail.html',
+#         {
+#             'post': post,
+#         }
+#     )
