c20f702d44f71ec665a4b7b0c03c41051efa41d3
김덕현 <kdh@Hyeons-MacBook-Air.local>
2022-12-18 13:59:04 +0900

    django-crispy-forms 적용하기

--- do_it_django_prj/settings.py
+++ do_it_django_prj/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
     'blog',
     'single_pages',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -129,3 +130,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, '_media')
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
