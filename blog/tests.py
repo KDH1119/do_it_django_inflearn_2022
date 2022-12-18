95a7d4e33c83944c50b8886a8ee159636da964d4
김덕현 <kdh@Hyeons-MacBook-Air.local>
2022-12-18 13:46:17 +0900

    포스트 수정 시 텍스트로 태그 지정하기

--- blog/tests.py
+++ blog/tests.py
@@ -62,8 +62,8 @@ class TestView(TestCase):
             content='Category가 없을 수도 있죠',
             author=self.user_obama,
         )
-        self.post_003.tags.add(self.tag_python)
         self.post_003.tags.add(self.tag_python_kor)
+        self.post_003.tags.add(self.tag_python)
 
 
     def navbar_test(self, soup):
@@ -244,11 +244,15 @@ class TestView(TestCase):
         main_area = soup.find('div', id='main-area')
         self.assertIn('Create a New Post', main_area.text)
 
+        tag_str_input = main_area.find('input', id='id_tags_str')
+        self.assertTrue(tag_str_input)
+
         self.client.post(
             '/blog/create_post/',
             {
                 'title': 'Post Form 만들기',
-                'content': 'Post Form 페이지를 만듭시다.'
+                'content': 'Post Form 페이지를 만듭시다.',
+                'tags_str': 'new tag; 한글 태그, python'
             }
         )
 
@@ -256,6 +260,12 @@ class TestView(TestCase):
         self.assertEqual(last_post.title, 'Post Form 만들기')
         self.assertEqual(last_post.author.username, 'obama')
         self.assertEqual(last_post.content, 'Post Form 페이지를 만듭시다.')
+
+        self.assertEqual(last_post.tags.count(), 3)
+        self.assertTrue(Tag.objects.get(name='new tag'))
+        self.assertTrue(Tag.objects.get(name='python'))
+        self.assertTrue(Tag.objects.get(name='한글 태그'))
+        self.assertEqual(Tag.objects.count(), 5)
 
     def test_update_post(self):
         update_post_url = f'/blog/update_post/{self.post_003.pk}/'
@@ -281,12 +291,17 @@ class TestView(TestCase):
         main_area = soup.find('div', id='main-area')
         self.assertIn('Edit Post', main_area.text)
 
+        tag_str_input = main_area.find('input', id='id_tags_str')
+        self.assertTrue(tag_str_input)
+        self.assertIn('python; 파이썬 공부', tag_str_input.attrs['value'])
+
         response = self.client.post(
             update_post_url,
             {
                 'title': '세 번째 포스트를 수정했습니다.',
                 'content': '안녕안녕안녕',
-                'category': self.category_music.pk
+                'category': self.category_music.pk,
+                'tags_str': '파이썬 공부; 한글 태그, some tag'
             },
             follow=True
         )
@@ -295,3 +310,8 @@ class TestView(TestCase):
         self.assertIn('세 번째 포스트를 수정했습니다.', main_area.text)
         self.assertIn('안녕안녕안녕', main_area.text)
         self.assertIn(self.category_music.name, main_area.text)
+
+        self.assertIn('파이썬 공부', main_area.text)
+        self.assertIn('한글 태그', main_area.text)
+        self.assertIn('some tag', main_area.text)
+        self.assertNotIn('python', main_area.text)
